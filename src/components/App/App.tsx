a45b804fd0e25fd9612242eb679a35de390f23d2
Robson dos Santos Silveira <216390@contact.grupocsu.local>
2022-01-26 20:00:15 -0300

    Atualizacao ate o video 2.1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TestComponent from '../TestComponent'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <TestComponent/> 
         </p>
         <a
           className="App-link"
