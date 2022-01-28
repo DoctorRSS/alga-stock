56e7f70e11ccf4fe39710974fd9f5f2fd5812864
DoctorRss <silver.rob16@yahoo.com.br>
2022-01-28 11:03:09 -0300

    Atualizacao ate o video 2.4

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <TestComponent/> 
+          <TestComponent name='Robson'/> 
           <ClassComponent name="Mundooo" />
         </p>
         <a
--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './TestComponent.css'
-const TestComponent = () => (
+
+
+function TestComponent(props: {name: string}) {
+    const[age,setAge] = useState(30)
+    return <div className="TestComponent" >Ola, {props.name}, {age}
+    <button onClick={() => {
+        setAge(age +1)
+    }}>
+
+    </button>
+    </div>
+}
+
+//Execução alternativa da solução acima. As duas geram o mesmo resultado
+/*const TestComponent = () => (
     <div className="TestComponent" >Test Component</div>
-)
+)*/
+
 
 export default TestComponent
