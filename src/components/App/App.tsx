bdbe63a68e18d252013939b360cb25dfd4aec110
DoctorRss <silver.rob16@yahoo.com.br>
2022-02-04 08:17:53 -0300

    Atualizacao ate o video 2.9

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import  React, {  useState } from 'react';
+import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import './App.css';
+import Input from '../../shared/Input';
 
 
 function TestComponent () {
@@ -10,6 +11,7 @@ function TestComponent () {
 }
 
 function App() {
+  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title="AlgaStock"/>
@@ -21,6 +23,12 @@ function App() {
         >
           Alert
         </Button>
+        <Input 
+        label="Street"
+        placeholder="E.g.: 15h Avenue"
+        value={street}
+        onChange={e => setStreet(e.target.value)}
+        />
       </Container>
     </div>
   );
