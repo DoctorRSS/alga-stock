414c7aa8f487a6aaa07c1b6c8e5cdec0e32415bc
DoctorRss <silver.rob16@yahoo.com.br>
2022-02-18 19:21:17 -0300

    Atualizacao ate o video 3.5

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import http from './utils/http';
+
+http.get('/posts')
+http.get('/posts/1')
+http.get('https://viacep.com.br/ws/29140160/json/')
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
