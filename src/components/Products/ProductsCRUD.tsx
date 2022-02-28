5f08ec7a84de8fa6880a5dbb2f4f68bdd4f5bdd0
DoctorRss <silver.rob16@yahoo.com.br>
2022-02-28 09:35:45 -0300

    Atualizacao ate o video 4.6

--- src/components/Products/ProductsCRUD.tsx
+++ src/components/Products/ProductsCRUD.tsx
@@ -8,7 +8,7 @@ import { Product } from '../../shared/Table/Table.mockdata'
 import ProductForm, { ProductCreator } from './ProductForm'
 import Swal from 'sweetalert2'
 import { connect, useDispatch } from 'react-redux'
-import { insertNewProduct } from '../../redux/Products/Products.actions'
+import { getProducts, insertNewProduct } from '../../redux/Products/Products.actions'
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -29,6 +29,7 @@ const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
   async function fetchData() {
     // const _products = await getAllProducts()
     // setProducts(_products)
+    dispatch(getProducts())
   }
 
   useEffect(() => {
