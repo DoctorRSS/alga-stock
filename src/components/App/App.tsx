fae5f16f4d3ffb66aa247c80c283dffa3ccf5fe7
DoctorRss <silver.rob16@yahoo.com.br>
2022-03-11 07:39:11 -0300

    Correcao de imports do reactdom para os componentes exact e Switch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import HomeView from '../../views/HomeView';
 import NotFoundView from '../../views/NotFoundView';
 
 
+//Orientacoes importantes: a palavra exact nao e mais necessaria a partir da v6 do react dom, pode remover ela e teremos o mesmo efeito desejado
+// O mesmo vale para a palavra conponent, que foi substituida pela palavra element
+// fonte https://stackoverflow.com/questions/69866581/property-exact-does-not-exist-on-type e https://reactrouter.com/docs/en/v6/upgrading/v5
+
+//Outra alteracao importante é o Switch ser substituipo pelo Routes nesta versao
+//Fonte https://ateliware.com/blog/react-router e https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={HomeView} />
-          <Route component={NotFoundView}/>
-        </Switch>
+        <Routes>
+          <Route path="/"  element={HomeView} />
+          <Route element={NotFoundView}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
